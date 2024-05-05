56ee5a9d414f9b3006732d238721b1e12e50c72d
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-05 22:35:13 +0500

    Chapter 1 to 5

---
+++ app.js
@@ -0,0 +1,131 @@
+
+                                                          //   Chapter 1
+
+
+           
+                                                          
+// // Question 1
+// alert('Hello JavaScript Family');
+
+
+
+
+// // Question 2
+// alert('Error! Please enter a valid password.');
+
+
+
+// // // Question 3
+// alert('Welcome to JS Land...');
+// alert('Happing Coding!');
+
+
+
+
+// // Question 4
+// alert('Hello... I can run JS through my web browsers console');
+
+
+
+
+                                                          //  Chapter 1
+
+
+
+
+
+
+
+                                                          //  Chapter 2
+
+
+// // Question 1
+
+// var username;
+
+// // Question 1
+
+
+
+
+
+// // Question 2
+
+// var myName = `Muhammad Hamza`;
+
+// // Question 2
+
+
+
+
+
+// // Question 3
+
+// var message = `Hello World`;
+// alert(message);
+
+// // Question 3
+
+
+
+
+
+// // Question 4
+
+// alert(`Muhammad Hamza`)
+// alert(`18 Year Old`)
+// alert(`Certified Web Developer`)
+
+// // Question 4
+
+
+
+
+
+// // Question 5
+
+// var msg = `Pizza\nPizz\nPiz\nPi\nP`;
+// alert(msg);
+
+// // Question 5
+
+
+
+
+// // Question 6
+
+// var email = `My email address is hamzaabro683@gmail.com`
+// alert(email)
+
+// // Question 6
+
+
+
+
+
+// // Question 7
+
+// var book = `A Smarter way to Learn JavaScript`
+// alert(`I am trying to learn from the book ${book}`)
+
+// // Question 7
+
+
+
+
+// // Question  8
+
+// const h2 = document.querySelector(`h2`)
+
+// h2.innerHTML = `Yah! I can write HTML content through Javascript`
+
+// // Question  8
+
+
+
+
+// Question 9
+
+
+
+// Question 9
